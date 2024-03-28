4ef6331aa0961371648665db6ad18ac8d38a2f58
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2024-03-28 18:16:34 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a ;
+    int b ;
+
+    std :: cin >> a;
+    std :: cin >> b;
+
+    std::cout << a + b << endl;
+
+    return 0;
+}
