861e61e6c7b1e260e3a7f30f05c3a772052b14ea
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2024-03-28 21:16:50 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    long long a;
+    long long b;
+    long long c;
+
+    std :: cin >> a >> b >> c;
+
+    long long sum = a+b+c;
+    std::cout << sum << endl;
+
+
+    return 0;
+}
