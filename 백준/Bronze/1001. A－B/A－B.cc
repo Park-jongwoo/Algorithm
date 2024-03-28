817390b1216e9f2606e2a66ffd958190be38a060
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2024-03-28 19:33:42 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a ;
+    int b ;
+
+    std :: cin >> a;
+    std :: cin >> b;
+
+    if ( 0 < a,b < 10)
+    {
+        std::cout << a - b << endl;
+    }
+    return 0;
+}
