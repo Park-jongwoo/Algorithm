67bd5bb21b545ca5717ab776822f43f28b5e8fe8
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2024-03-28 21:43:23 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+
+
+    std::cout << "|\\_/|" << endl;
+    std::cout << "|q p|   /}" << endl;
+    std::cout << "( 0 )\"\"\"\\" << endl;
+    std::cout << "|\"^\"`    |" << endl;
+    std::cout << "||_/=\\\\__|" << endl;
+    return 0;
+}
