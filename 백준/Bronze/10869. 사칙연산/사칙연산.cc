e035528f58d9e2796da32adbd5b0730ed6e8b318
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2024-03-28 20:05:57 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a ;
+    int b ;
+    
+    std :: cin >> a;
+    std :: cin >> b;
+
+    std::cout << a+b << endl;
+    std::cout << a-b << endl;
+    std::cout << a*b << endl;
+    std::cout << a/b << endl;    
+    std::cout << a%b << endl;
+    return 0;
+}
