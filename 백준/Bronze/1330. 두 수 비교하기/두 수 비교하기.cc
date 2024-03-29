9c67381980d99374359a3b48139bd8b820ef7dd4
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2024-03-29 13:06:18 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a ;
+    int b ;
+    
+    cin >> a >> b;
+    
+
+    if (a < b)
+    {
+        cout << "<" << endl;
+    }
+    else if (a > b)
+    {
+        cout << ">" << endl;   
+    }
+    else if ( a == b )
+    {
+        cout << "==" << endl;        
+    }
+    
+
+
+    return 0;
+}
