8b1a2f0b6e004b4abdcdf91c2e26cef6c738583e
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2024-03-29 15:13:28 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int input;
+
+    cin >> input;
+
+    if ( input % 4 == 0 && (input%100 != 0 || input%400 == 0))
+    {
+        cout << "1" << endl;
+    }
+    else
+    {
+        cout << "0" << endl;        
+    }
+    return 0;
+}
