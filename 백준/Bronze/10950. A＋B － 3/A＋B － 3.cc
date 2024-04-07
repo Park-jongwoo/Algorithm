68c78a4227e928ca3d0b3ee8e409c913356a628d
박종우 <56357706+Park-jongwoo@users.noreply.github.com>
2024-04-08 02:07:36 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int T;
+    int a;
+    int b;
+    cin >> T;
+
+    for(int i = 0 ; i<T; ++i )
+    {
+        cin >> a >> b;
+        cout << a+b <<endl;
+    }
+
+    return 0;
+}
